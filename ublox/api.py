69af606db495b469fba405182302f2fa5520f445
Adam Dodge <adam.dodge@colorado.edu>
2019-06-26 17:36:33 -0600

    Save data if connection breaks.

--- ublox/api.py
+++ ublox/api.py
@@ -10,7 +10,7 @@ def call_send(url, key, data):
     """ Function for calling send and checking if packet is sent. This is threaded to speed up data collection. """
     count = 0
     fname = '/home/ccaruser/not-sent/' + url[-6:] + '.bin'
-    print(fname)
+    print(url)
     while not send(url, key, data) and count < 100:
         count += 1
     if count == 100:
