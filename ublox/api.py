569288e924f5432ef85d63b7cfa8a4667ed6fd41
Adam Dodge <adam.dodge@colorado.edu>
2019-07-18 08:15:41 -0600

    New diskcache method.

--- ublox/api.py
+++ ublox/api.py
@@ -6,11 +6,14 @@ import numpy as np
 
 
 def save_to_dc(cache, t, data):
+    """ Function for saving a measurement to the diskcache. """
     cache[bytes(str(t), 'utf-8')] = data
 
 
 def send_old(cache, url, key):
+    """ Function for sending old data saved to the diskcache when the program couldn't connect to the web server. """
     for i in cache:
+        print(i, cache[i])
         for j in range(10):
             if send(url, key, cache[i], 'Old '):
                 del cache[i]
