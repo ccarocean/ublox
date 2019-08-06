bc99e091ad01e9dc1e8c8feec66120cda9d94891
Adam Dodge <adam.dodge@colorado.edu>
2019-08-06 15:50:13 -0600

    Fixing initialization

--- ublox/_main.py
+++ ublox/_main.py
@@ -53,8 +53,6 @@ def main():
     parser.add_argument('--led', type=int, default=20, help='LED pin. Default is 20.')
     args = parser.parse_args()
 
-    print(args.configfile)
-
     if args.comm == "USB":
         port = '/dev/serial/by-id/usb-u-blox_AG_-_www.u-blox.com_u-blox_GNSS_receiver-if00'  # Serial port  TODO: UART
     elif args.comm == "UART":
@@ -73,8 +71,6 @@ def main():
     # Configure
     config = ConfigParser(inline_comment_prefixes=('#', ';'))
     config.read(args.configfile)  # Read configuration packets to be sent
-
-    print(config)
 
     packet = CfgValsetSend(config[args.comm])  # Create configuration packets
 
